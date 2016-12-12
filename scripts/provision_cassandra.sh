9cf554aaadfcc1f4363deaf7d6c20d5ebf151648
Edward Viaene <ward.viaene@gmail.com>
2016-12-12 09:08:06 +0100

    cassandra version bump

--- scripts/provision_cassandra.sh
+++ scripts/provision_cassandra.sh
@@ -26,10 +26,10 @@ apt-get install -y oracle-java8-installer
 echo "Downloading Cassandra"
 mkdir -p /opt
 cd /opt
-wget -q http://www-us.apache.org/dist/cassandra/3.0.5/apache-cassandra-3.0.5-bin.tar.gz
+wget -q http://www-us.apache.org/dist/cassandra/3.0.10/apache-cassandra-3.0.10-bin.tar.gz
 echo "Extracting Cassandra"
-tar -xzf apache-cassandra-3.0.5-bin.tar.gz
-cd apache-cassandra-3.0.5
+tar -xzf apache-cassandra-3.0.10-bin.tar.gz
+cd apache-cassandra-3.0.10
 
 echo "initial setup"
 groupadd cassandra
@@ -38,6 +38,6 @@ mkdir -p /var/log/cassandra
 chown -R cassandra /var/log/cassandra
 mkdir -p /var/lib/cassandra
 chown -R cassandra /var/lib/cassandra
-chown -R cassandra /opt/apache-cassandra-3.0.5
-cat /vagrant/conf/cassandra.yaml | sed "s/CLUSTER_NAME/${CLUSTER_NAME}/" | sed "s/SEED_LIST/${SEED_LIST}/" > /opt/apache-cassandra-3.0.5/conf/cassandra.yaml
-su - cassandra -c '/opt/apache-cassandra-3.0.5/bin/cassandra'
+chown -R cassandra /opt/apache-cassandra-3.0.10
+cat /vagrant/conf/cassandra.yaml | sed "s/CLUSTER_NAME/${CLUSTER_NAME}/" | sed "s/SEED_LIST/${SEED_LIST}/" > /opt/apache-cassandra-3.0.10/conf/cassandra.yaml
+su - cassandra -c '/opt/apache-cassandra-3.0.10/bin/cassandra'
